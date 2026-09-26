372b4358a2fe57f0bc3aafc9f406c4b9b7c571a9
Reiss76 <raulreiss76@gmail.com>
2026-09-26 13:48:30 -0600

    fix: corregir URL de API - /public/ no /api/public/

--- js/product.js
+++ js/product.js
@@ -166,7 +166,7 @@ async function loadProduct() {
 
   let products;
   try {
-    const res = await fetch(MJ_API_BASE + '/api/public/shop/mahjoy/products');
+    const res = await fetch(MJ_API_BASE + '/public/shop/mahjoy/products');
     if (!res.ok) throw new Error('API error');
     const data = await res.json();
     products = (data.products || []).map(p => ({
